09e5c990bc6373e54da045e514b27ae495e1ebf6
davidkov111 <kovacsdavid648@outlook.com>
2024-05-30 23:58:07 +0300

    SolanaMysteryBoxShop

--- CoreMysteryBox/settings.py
+++ CoreMysteryBox/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #False #True #False #True #False
+DEBUG = False #True #False #True #False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
